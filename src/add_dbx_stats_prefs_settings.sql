f50f24987ab0b4dbab222d907bd78dc5fe080d99
dbdevmaster <97837250+dbdevmaster@users.noreply.github.com>
2024-07-16 18:14:29 +0200

    Update add_dbx_stats_prefs_settings.sql

--- src/add_dbx_stats_prefs_settings.sql
+++ src/add_dbx_stats_prefs_settings.sql
@@ -11,11 +11,11 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE(dbx_prefs_manager.add_pname('DEGREE', 'Y'));
     DBMS_OUTPUT.PUT_LINE(dbx_prefs_manager.add_pname('ESTIMATE_PERCENT', 'N'));
     DBMS_OUTPUT.PUT_LINE(dbx_prefs_manager.add_pname('GLOBAL_TEMP_TABLE_STATS', 'N'));
-    DBMS_OUTPUT.PUT_LINE(dbx_prefs_manager.add_pname('GRANULARITY', 'N'));
+    DBMS_OUTPUT.PUT_LINE(dbx_prefs_manager.add_pname('GRANULARITY', 'Y'));
     DBMS_OUTPUT.PUT_LINE(dbx_prefs_manager.add_pname('INCREMENTAL', 'Y'));
     DBMS_OUTPUT.PUT_LINE(dbx_prefs_manager.add_pname('INCREMENTAL_STALENESS', 'N'));
     DBMS_OUTPUT.PUT_LINE(dbx_prefs_manager.add_pname('INCREMENTAL_LEVEL', 'N'));
-    DBMS_OUTPUT.PUT_LINE(dbx_prefs_manager.add_pname('METHOD_OPT', 'N'));
+    DBMS_OUTPUT.PUT_LINE(dbx_prefs_manager.add_pname('METHOD_OPT', 'Y'));
     DBMS_OUTPUT.PUT_LINE(dbx_prefs_manager.add_pname('NO_INVALIDATE', 'N'));
     DBMS_OUTPUT.PUT_LINE(dbx_prefs_manager.add_pname('OPTIONS', 'Y'));
     DBMS_OUTPUT.PUT_LINE(dbx_prefs_manager.add_pname('PREFERENCE_OVERRIDES_PARAMETER', 'N'));
@@ -26,4 +26,3 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE(dbx_prefs_manager.add_pname('WAIT_TIME_TO_UPDATE_STATUS', 'N'));
 END;
 /
-
